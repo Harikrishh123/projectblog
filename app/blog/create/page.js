b2cc6ef0d58c34979a12369e6fde3c80345db841
Harikrishh123 <nvsatyamnwelamsetti@gmail.com>
2025-02-08 02:08:23 +0530

    first

--- app/blog/create/page.js
+++ app/blog/create/page.js
@@ -60,12 +60,12 @@ const create = () => {
     }
 
   return (
-<>{!loading &&     <section className="bg-gray-700">
+<>{!loading &&     <section className=" bg-gray-700">
         <div className="bg-white h-1 opacity-10">
         </div>
-  <div className="flex flex-col items-center justify-center px-6 pb-10 mx-auto md:h-screen lg:py-0 "  >
+  <div className="flex flex-col items-center justify-center px-6 pb-8 mx-auto md:h-screen lg:py-0 "  >
     
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="container w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 pb-4 pt-4 md:pb-6 md:pt-6">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Create Your Blog
@@ -81,7 +81,7 @@ const create = () => {
                   </div>
                   <div>
                       <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter the description:</label>
-                      <textarea rows="8" name="description" id="description" onChange={onchange} value={details.description} placeholder="Leave a description (Minimum of length 25)" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                      <textarea rows="5" name="description" id="description" onChange={onchange} value={details.description} placeholder="Leave a description (Minimum of length 25)" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                   </div>
                   <div>
                       
--- app/blog/edit/[id]/page.js
+++ app/blog/edit/[id]/page.js
@@ -97,7 +97,7 @@ const edit = () => {
                   </div>
                   <div>
                       <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter the description:</label>
-                      <textarea rows="8" name="description" id="description" onChange={onchange} value={details.description} placeholder="Leave a description (Minimum of length 25)" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                      <textarea rows="5" name="description" id="description" onChange={onchange} value={details.description} placeholder="Leave a description (Minimum of length 25)" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                   </div>
                   <div>
                       
