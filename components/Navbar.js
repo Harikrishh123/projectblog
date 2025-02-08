f148b6fddcb344e9066a130abb8e36c53ff3dc8b
Harikrishh123 <nvsatyamnwelamsetti@gmail.com>
2025-02-08 09:38:34 +0530

    navbard

--- components/Navbar.js
+++ components/Navbar.js
@@ -35,7 +35,7 @@ const Navbar = () => {
   return (
 
    <>
-   {lgout ? <nav className="bg-white border-gray-200 dark:bg-gray-900">
+   {lgout ? <nav className=" border-gray-200 bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
       <img src="/favicon.ico" className="h-8" alt="Flowbite Logo" />
