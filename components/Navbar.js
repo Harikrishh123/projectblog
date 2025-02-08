b8e24eddaaa30865fdb41c773505dd79bb024c9e
Harikrishh123 <nvsatyamnwelamsetti@gmail.com>
2025-02-08 09:56:29 +0530

    navb

--- components/Navbar.js
+++ components/Navbar.js
@@ -39,17 +39,17 @@ const Navbar = () => {
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
       <img src="/favicon.ico" className="h-8" alt="Flowbite Logo" />
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Postblog  </span>
+      <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Postblog  </span>
   </Link>
   <div className="flex md:order-2">
     <div className=" hidden relative md:block">
 
       <div className='flex gap-2'>
-  <Link href="/login" className="block py-2 px-3 mx-2 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Signin</Link>
-  <Link href="/signup" className="block py-2 px-3 mx-2 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Signup</Link>       </div>
+  <Link href="/login" className="block py-2 px-3 mx-2 rounded-sm   md:p-0 md:hover:text-blue-500 text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">Signin</Link>
+  <Link href="/signup" className="block py-2 px-3 mx-2 rounded-sm   md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">Signup</Link>       </div>
       
     </div>
-    <button onClick={() => {showDropdown ? setshowDropdown(false) : setshowDropdown(true)}} data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
+    <button onClick={() => {showDropdown ? setshowDropdown(false) : setshowDropdown(true)}} data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden ocus:outline-none focus:ring-2  text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
@@ -60,21 +60,21 @@ const Navbar = () => {
       <div className="relative mt-3 md:hidden">
 
         <div className='flex gap-2 md: flex-col'>
-<Link href="/login" className="block py-2 px-3 mx-2 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Signin</Link>
-<Link href="/signup" className="block py-2 px-3 mx-2 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Signup</Link>      
+<Link href="/login" className="block py-2 px-3 mx-2 rounded-sm   md:p-0 md:hover:text-blue-500 text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">Signin</Link>
+<Link href="/signup" className="block py-2 px-3 mx-2 rounded-sm   md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">Signup</Link>      
  </div>
       </div>
 
     </div>
   </div>
-</nav> :     <nav className="bg-white border-gray-200 dark:bg-gray-900">
+</nav> :     <nav className=" border-gray-200 bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
       <img src="/favicon.ico" className="h-8" alt="Flowbite Logo" />
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Postblog  </span>
+      <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Postblog  </span>
   </Link>
   <div className="flex md:order-2">
-    <button type="button" onClick={() => {showDropdown ? setshowDropdown(false) : setshowDropdown(true)}} data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+    <button type="button" onClick={() => {showDropdown ? setshowDropdown(false) : setshowDropdown(true)}} data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden  text-gray-400  focus:outline-none focus:ring-4 focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
       <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
       </svg>
@@ -82,20 +82,20 @@ const Navbar = () => {
     </button>
     <div className=" hidden relative md:block">
       <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+        <svg className="w-4 h-4  text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
         </svg>
         <span className="sr-only">Search icon</span>
       </div>
 
       <div className='flex gap-2' role='search'>
-      <input type="search" id="search-navbar" onChange={(e) => setquery(e.target.value)} className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." aria-label='search'/>
+      <input type="search" id="search-navbar" onChange={(e) => setquery(e.target.value)} className="block w-full p-2 ps-10 text-sm border  rounded-lg  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Search..." aria-label='search'/>
 
-      <Link href = {pathname !== "/" ? `/search/myblog/${query}` : `/search/${query}`  }><button onClick={handleSearch} type="button" className="text-gray-900 p-2 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm  text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Search</button></Link>
+      <Link href = {pathname !== "/" ? `/search/myblog/${query}` : `/search/${query}`  }><button onClick={handleSearch} type="button" className="p-2  border focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm  text-center border-gray-600 text-gray-400 hover:text-white hover:bg-gray-600 focus:ring-gray-800">Search</button></Link>
       </div>
       
     </div>
-    <button onClick={() => {showDropdown ? setshowDropdown(false) : setshowDropdown(true)}} data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
+    <button onClick={() => {showDropdown ? setshowDropdown(false) : setshowDropdown(true)}} data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden  focus:outline-none focus:ring-2  text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
@@ -105,28 +105,28 @@ const Navbar = () => {
     <div className={`items-center justify-between ${!showDropdown ? "hidden" : ""} w-full md:flex md:w-auto md:order-1" id="navbar-search`}>
       <div className="relative mt-3 md:hidden">
         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-          <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+          <svg className="w-4 h-4  text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
           </svg>
         </div>
         <div className='flex gap-2'>
-        <input type="search" id="search-navbar" onChange={(e) => setquery(e.target.value)} className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." aria-label='search'/>
+        <input type="search" id="search-navbar" onChange={(e) => setquery(e.target.value)} className="block w-full p-2 ps-10 text-sm  border  rounded-lg  border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Search..." aria-label='search'/>
 
-<Link href = {pathname !== "/" ? `/search/myblog/${query}` : `/search/${query}`  }><button onClick={handleSearch} type="button" className="text-gray-900 p-2 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm  text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Search</button></Link>
+<Link href = {pathname !== "/" ? `/search/myblog/${query}` : `/search/${query}`  }><button onClick={handleSearch} type="button" className=" p-2  border   focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm  text-center border-gray-600 text-gray-400 hover:text-white hover:bg-gray-600 focus:ring-gray-800">Search</button></Link>
       </div>
       </div>
-      <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  bg-gray-800 md:bg-gray-900 border-gray-700">
         <li>
-          <Link href="/" className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Allblogs</Link>
+          <Link href="/" className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent  md:p-0 md:text-blue-500" aria-current="page">Allblogs</Link>
         </li>
         <li>
-          <Link href="/blog/create" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Create a blog</Link>
+          <Link href="/blog/create" className="block py-2 px-3 rounded-sm   md:p-0 md:hover:text-blue-500 text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">Create a blog</Link>
         </li>
         <li>
-          <Link href="/myblog" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Myblogs</Link>
+          <Link href="/myblog" className="block py-2 px-3 rounded-sm   md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">Myblogs</Link>
         </li>
         <li>
-          <button onClick={logout} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Logout</button>
+          <button onClick={logout} className="block py-2 px-3 rounded-sm   md:p-0 text-white md:hover:text-red-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">Logout</button>
         </li>
       </ul>
     </div>
